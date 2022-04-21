9e7afebd6c259fb56b4739fc1a1b51f9e4d07456
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-21 17:03:19 +0500

    route handling nodejs

--- routing_handling_nodejs/index.js
+++ routing_handling_nodejs/index.js
@@ -2,18 +2,18 @@ const http = require("http");
 const url = require("url");
 const server = http.createServer((req, res) => {
     // console.log(req.url);
-    if (req.url=='/') {
+    if (req.url == '/') {
         res.end("hello from the home side nabeel");
-        
-    }else if(req.url=='/about'){
+
+    } else if (req.url == '/about') {
         res.end('hello from the about side');
-    }else if(req.url=='/contact'){
+    } else if (req.url == '/contact') {
         res.end('hello from the contact side');
-    }else{
-        res.writeHead(404,{"Content-type":"text/html"});
+    } else {
+        res.writeHead(404, { "Content-type": "text/html" });
         res.end('<h1>error 404 not fount</h1>');
     }
 });
 server.listen(8000, "127.0.0.1", () => {
-  console.log("listening to the port 8000");
+    console.log("listening to the port 8000");
 });

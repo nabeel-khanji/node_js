85749374eb3e1c0a95bb702756c8b56e88d07fd5
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-22 15:05:35 +0500

    stream pipe

---
+++ stream_pipe/main.js
@@ -0,0 +1,30 @@
+// readble - stream which is used for read operation
+// writeable - stream which is used for write operation
+// duplex - stream which can be used for both read and write operation
+// transform - A type of duplex stream where the output is computed based on the input
+import fs from 'fs';
+import http from 'http';
+const server = http.createServer();
+server.on('request', (req, res) => {
+    //     fs.readFile('input.txt', (err, data) => {
+    //         if (err) return console.error(err);
+    //         res.end(data.toString());
+    //     })
+    const rstream = fs.createReadStream('input.txt');
+
+    // rstream.on('data', (chunkdata) => {
+    //     res.end(chunkdata)
+    // })
+    // rstream.on('end', () => {
+    //     res.end()
+    // })
+    // rstream.on('error', (err) => {
+    //     console.log(err);
+    //     res.end('file not found')
+    // })
+    rstream.pipe(res);
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listen to port number 8000');
+});
